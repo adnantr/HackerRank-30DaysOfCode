8f9895045c326662a4c221c3d44728ae79342903
Adnan TATAR <tatar38adnan@hotmail.com>
2022-08-26 23:42:56 +0300

    day3

--- HackerRankDay3/Program.cs
+++ HackerRankDay3/Program.cs
@@ -17,9 +17,30 @@ namespace HackerRankDay3
 {
     internal class Program
     {
+
+
         public static void Main(string[] args)
         {
             int N = Convert.ToInt32(Console.ReadLine().Trim());
+            if (N % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            if (N % 2 == 0)
+            {
+                if (2 < N && N < 5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                if (6 < N && N <= 20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                if (N > 20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
         }
     }
 
