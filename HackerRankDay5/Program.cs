6cb3b22773ecd1f5bb17748e669580e31c6a7588
Adnan TATAR <tatar38adnan@hotmail.com>
2022-08-28 12:08:13 +0300

    Day5

---
+++ HackerRankDay5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HackerRankDay5
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i= 1; i <= 10; i++)
+            {
+                int result = (n * i);
+                Console.WriteLine("{0} x {1} = {2}",n,i,result);
+            }
+        }
+    }
+
+}
