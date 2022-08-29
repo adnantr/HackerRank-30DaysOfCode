887f20d79b94ab03c469b5f285d6c533ae1b5f1a
Adnan TATAR <tatar38adnan@hotmail.com>
2022-08-29 13:56:48 +0300

    Day6

---
+++ HackerRankDay6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HackerRankDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T, count=0;
+            T = Convert.ToInt32(Console.ReadLine());
+            while (count < T)
+            {
+                count++;
+                string text = Console.ReadLine();
+                for (int i = 0; i < text.Length; i += 2)
+                {
+                    Console.Write(text[i]);
+                }
+                Console.Write(" ");
+                for (int j = 1; j < text.Length; j += 2)
+                {
+                    Console.Write(text[j]);
+                }
+                Console.WriteLine();
+            }
+            
+
+
+        }
+    }
+}
